05216ceecc318f48cfe2f031d54b903d6b8aa11d
Lucas Colombo <lucasncolombo@gmail.com>
2024-01-03 10:16:20 -0300

    🍾 Initial Commit

---
+++ haunted_houses/hello/hello_controller.py
@@ -0,0 +1,14 @@
+from pest import controller, get
+
+from .greeter_service import Greet, GreeterService
+
+
+@controller("/")
+class HelloController:
+    """say hello routes"""
+    service: GreeterService  # 💉 injected
+
+    @get("/")
+    def say_hello(self) -> Greet:
+        """Say hello"""
+        return self.service.say_hello()
